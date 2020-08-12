7e061f3a2a5988f0f5a2b19a9fd2959cac891561
dicky08 <dickyf147@gmail.com>
2020-08-12 12:37:27 +0700

    run

--- tugas2_introJavascript2/3.js
+++ tugas2_introJavascript2/3.js
@@ -1,6 +1,12 @@
 // SELEKSI NILAI
 function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
-  if (nilaiAwal > nilaiAkhir) {
+  if (
+    typeof nilaiAwal !== "number" ||
+    typeof nilaiAkhir !== "number" ||
+    typeof dataArray !== "object"
+  ) {
+    return "Tipe data salah";
+  } else if (nilaiAwal > nilaiAkhir) {
     return "Nilai akhir harus lebih besar dari Nilai Awal";
   } else if (dataArray.length <= 5) {
     return "data array kurang";
@@ -8,7 +14,10 @@ function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
     let arayBaru = dataArray.filter((data) => {
       return data > nilaiAwal && data < nilaiAkhir;
     });
+    if (arayBaru < 1) {
+      return "Data array tidak ada";
+    }
     return arayBaru.sort((a, b) => a - b);
   }
 }
-console.log(seleksiNilai(25, 20, [2, 25, 2, 14, 17, 30, 8]));
+console.log(seleksiNilai(5, 20, [2, 25, 2, 30, 8, 2]));
